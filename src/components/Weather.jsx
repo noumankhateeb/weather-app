fe96c98b6f1c5f3774a6ce1984f1241e138b0370
noumankhateeb <noumankhateebnk@gmail.com>
2024-12-26 14:45:30 +0500

    first commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,76 +1,153 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BiSearch } from 'react-icons/bi'
-import { BsCloud } from 'react-icons/bs'
 import { FaLocationDot, FaWind } from 'react-icons/fa6'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchForecastByCity } from '../redux/weatherSlice'
+import sunnyImage from '../assets/sunny.jpg'
+import rainImage from '../assets/rain.jpg'
+import snowImage from '../assets/snow.jpg'
+import cloudImage from '../assets/cloud.jpg'
 
 const Weather = () => {
-  return (
-    <div className='weather-container'>
 
-        <div className="main-section">
+    const [city, setCity] = useState("");
 
-            <div className="weather-info">
-                <div className="location">
-                    <h3>New York - USA</h3>
+    const handleSearch = () => {
+        if (city.trim() !== "") {
+            dispatch(fetchForecastByCity(city))
+        }
+    }
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchForecastByCity("New York"));
+    }, [dispatch])
+
+    const forecast = useSelector((state) => state.weather.forecast);
+    console.log(forecast);
+
+    const forecastHours = forecast?.forecast?.forecastday[0]?.hour.slice(0, 10);
+
+    const weatherCondition = forecast?.current?.condition?.text?.toLowerCase();
+
+    let backgroundImage = sunnyImage;
+
+    if (weatherCondition) {
+        if (weatherCondition.includes("sunny") || weatherCondition.includes("clear")) {
+            backgroundImage = sunnyImage
+        } else if (weatherCondition.includes("rain")) {
+            backgroundImage = rainImage
+        } else if (weatherCondition.includes("snow")) {
+            backgroundImage = snowImage
+        } else if (weatherCondition.includes("cloud") || weatherCondition.includes("overcast")) {
+            backgroundImage = cloudImage
+        }
+    }
+
+    return (
+        <div
+            className='weather-container'
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+            }}
+        >
+
+            <div className="main-section">
+
+                <div className="weather-info">
+                    <div className="location">
+                        <h3>
+                            {forecast?.location?.name} - {forecast?.location?.country}
+                        </h3>
+                    </div>
+                    <div className="condition">
+                        <h1>{forecast?.current?.condition?.text}</h1>
+                    </div>
                 </div>
-                <div className="condition">
-                    <h1>Overcast</h1>
+
+                <div className="weather-hours">
+                    {forecastHours?.map((hour, index) => {
+                        const time = new Date(hour.time).toLocaleTimeString("en-GB", {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                            hour12: false,
+                        })
+                        return (
+                            <div className="hour-card">
+                                <div className="hour-time">
+                                    <p>{time}</p>
+                                </div>
+                                <div className="hour-condition">
+                                    <img src={hour?.condition.icon} alt="" />
+                                </div>
+                                <div className="hour-temp">
+                                    <h2>{Math.ceil(hour?.temp_c)}°C</h2>
+                                </div>
+                            </div>
+                        )
+                    })}
+
                 </div>
+
             </div>
 
-            <div className="weather-hours">
-                <div className="hour-card">
-                    <div className="hour-time">
-                        <p>09:00</p>
-                    </div>
-                    <div className="hour-condition">
-                        <BsCloud />
-                    </div>
-                    <div className="hour-temp">
-                        <h2>10*C</h2>
-                    </div>
+            <div className="side-section">
+                <div className="search-box">
+                    <FaLocationDot className='icon' />
+                    <input
+                        type="text"
+                        placeholder={forecast?.location?.name}
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <BiSearch className='icon' onClick={handleSearch} />
+                </div>
+
+                <div className="temp-info">
+                    <h1>{Math.ceil(forecast?.current?.temp_c)}°C</h1>
+                    <p>
+                        <FaWind /> {forecast?.current?.wind_dir} {forecast?.current?.wind_kph} km/h
+                    </p>
+                </div>
+
+                <div className="forecast-days">
+                    <h1 className='forecast-heading'>The Next Days Forecast</h1>
+                    {forecast?.forecast?.forecastday?.map((item, index) => {
+
+                        const forecastDate = new Date(item.date).toLocaleDateString("en-GB", {
+                            weekday: "long",
+                            day: "2-digit",
+                            month: "long"
+                        })
+
+                        return (
+                            <div className="forecast-item" key={index}>
+                                <div className="forecast-details">
+                                    <div className="forecast-icon">
+                                        <img src={item.day.condition.icon} alt="" />
+                                    </div>
+                                    <div className="details">
+                                        <h2>{forecastDate}</h2>
+                                        <p>{item.day.condition.text}</p>
+                                    </div>
+                                </div>
+                                <div className="forecast-temp">
+                                    <div className="temp-display">
+                                        <h2>{Math.ceil(item.day.maxtemp_c)}°C</h2>
+                                        <h2>{Math.ceil(item.day.mintemp_c)}°C</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+
                 </div>
             </div>
-
         </div>
-
-        <div className="side-section">
-            <div className="search-box">
-                <FaLocationDot className='icon' />
-                <input type="text" placeholder='New York' />
-                <BiSearch className='icon' />
-            </div>
-
-            <div className="temp-info">
-                <h1>10*C</h1>
-                <p>
-                    <FaWind /> NE 40 km/h
-                </p>
-            </div>
-
-            <div className="forecast-days">
-                <h1 className='forecast-heading'>The Next Days Forecast</h1>
-                <div className="forecast-item">
-                    <div className="forecast-details">
-                        <div className="forecast-icon">
-                            <BsCloud />
-                        </div>
-                        <div className="details">
-                            <h2>monday, December 16</h2>
-                            <p>overcast</p>
-                        </div>
-                    </div>
-                    <div className="forecast-temp">
-                        <div className="temp-display">
-                            <h2>10*C</h2>
-                            <h2>5*C</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Weather
--- src/redux/weatherSlice.js
+++ src/redux/weatherSlice.js
@@ -1,15 +1,32 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from "axios"
 
-const API_KEY = "e316ccd0603344ac8ce214044242512"
-const BASE_URL  = "http://api.weatherapi.com/v1"
+const API_KEY = "e316ccd0603344ac8ce214044242512";
+const BASE_URL = "http://api.weatherapi.com/v1";
 
 export const fetchForecastByCity = createAsyncThunk(
     "weather/fetchForecastByCity",
-    async (city)= {
+    async (city) => {
         const response = await axios.get(
             `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=5`
         );
         return response.data;
     }
 );
+
+const weatherSlice = createSlice({
+    name:"weather",
+    initialState:{
+        forecast:{},
+        status: "idle",
+        error: null
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchForecastByCity.fulfilled, (state, action)=>{
+            state.forecast = action.payload
+        })
+    }
+})
+
+export default weatherSlice.reducer;
